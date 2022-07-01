7e05267321d532d0d9600d3bd376ba3af348f1fb
Nico-T-Ihle <n.ihle2003@gmail.com>
2022-07-01 19:35:02 +0200

    Initial Commit

---
+++ OpenMind/OpenMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMindApp.swift
+//  OpenMind
+//
+//  Created by Nico Ihle on 01.07.22.
+//
+
+import SwiftUI
+
+@main
+struct OpenMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
